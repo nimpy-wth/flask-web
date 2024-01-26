1105e7f299a7f29265d562d9f3d5acac03942768
Nitasneem Liala <6610110160@email.psu.ac.th>
2024-01-26 11:03:26 +0700

    add flash message

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
@@ -19,14 +19,15 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            pass
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(user_name) < 2:
-            pass 
+            flash('username must be greater than 1 character.', category='error')
         elif password1 != password2:
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            pass
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            pass
+            flash('Account created!', category='success')
+
 
     return render_template("signup.html")
