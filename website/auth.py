79f783f3ec8448d5832877f9a438436554816355
Nitasneem Liala <6610110160@email.psu.ac.th>
2024-01-30 16:30:51 +0700

    log out

--- website/auth.py
+++ website/auth.py
@@ -16,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -25,8 +26,10 @@ def login():
     return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
