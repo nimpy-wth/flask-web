8a5185ad5792fb6f9175e0acecfc6fe1dc2f81b3
Nitasneem Liala <6610110160@email.psu.ac.th>
2024-02-11 21:51:37 +0700

    Main File

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from website import create_app
+from flask import Flask
 
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app = create_app()
 
 if __name__ == '__main__':
